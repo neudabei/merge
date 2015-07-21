05ab4c9a8082c78f1d80e45a25678e7a82e1e4b7
neudabei <rschultzejena@gmail.com>
2015-07-21 22:27:21 +0100

    merge logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def merge_csvs(path_to_folder)
+
+    require 'csv'
+
+    master = CSV.read("#{path_to_folder}/master.csv") # Reads in master # -> datei.csv.current_path didn't get the actual file
+    master.each {|each| each.push('')} # Adds another column to all rows
+    Dir.glob("#{path_to_folder}/*.csv").each do |each| #Goes thru all csv files
+      next if each == 'master.csv' # skips the master csv file
+      file = CSV.read(each) # Reads in each file
+      file.each do |line| #Goes thru each line of the file
+        temp = master.assoc(line[0]) # Finds the appropriate line in master
+        temp[-1] = line[1] if temp #updates last column if line is found
+      end
+    end
+
+    csv = CSV.open("#{path_to_folder}/output.csv",'wb') #opens output csv file for writing
+    master.each {|each| csv << each} #Goes thru modified master and saves it to file
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-      # uploaded_io = params[:user][:filename]
-      # File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
-      #   file.write(uploaded_io.read)
-      # end
-
-
     if @user.save
       session[:user_id] = @user.id
 
@@ -28,29 +22,39 @@ class UsersController < ApplicationController
       end
       flash[:notice] = "You are registered."
       redirect_to upload_path 
+
     else
       render :new
     end
-  end
 
-  def merge
-
-    require 'csv'
-    master = CSV.read('uploads/spreadsheet/csv/24/master.csv') # Reads in master
-    master.each {|each| each.push('')} # Adds another column to all rows
-    Dir.glob('uploads/spreadsheet/csv/24/*.csv').each do |each| #Goes thru all csv files
-      next if each == 'master.csv' # skips the master csv file
-      file = CSV.read(each) # Reads in each one
-      file.each do |line| #Goes thru each line of the file
-        temp = master.assoc(line[0]) # Finds the appropriate line in master
-        temp[-1] = line[1] if temp #updates last column if line is found
-      end
-    end
-
-    csv = CSV.open('output.csv','wb') #opens output csv file for writing
-    master.each {|each| csv << each} #Goes thru modified master and saves it to file
+    #merge method from application_controller.rb
+    merge_csvs("public/uploads/spreadsheet/csv/#{@user.spreadsheets.first.user_id}")
 
   end
+
+  # def merge
+
+  #   @user = current_user
+
+  #   require 'csv'
+    
+
+  #     master = CSV.read("public/uploads/spreadsheet/csv/#{@user.spreadsheets.first.user_id}/master.csv") # Reads in master # -> datei.csv.current_path didn't get the actual file
+  #     master.each {|each| each.push('')} # Adds another column to all rows
+  #     Dir.glob("public/uploads/spreadsheet/csv/#{@user.spreadsheets.first.user_id}/*.csv").each do |each| #Goes thru all csv files
+  #       next if each == 'master.csv' # skips the master csv file
+  #       file = CSV.read(each) # Reads in each file
+  #       file.each do |line| #Goes thru each line of the file
+  #         temp = master.assoc(line[0]) # Finds the appropriate line in master
+  #         temp[-1] = line[1] if temp #updates last column if line is found
+  #       end
+  #     end
+
+  #     csv = CSV.open("public/uploads/spreadsheet/csv/#{@user.spreadsheets.first.user_id}/output.csv",'wb') #opens output csv file for writing
+  #     master.each {|each| csv << each} #Goes thru modified master and saves it to file
+
+
+  # end
 
   def user_params
     params.require(:user).permit(:email, :password, spreadsheets_attributes: [:id, :user_id, :csv])
